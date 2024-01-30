5018e1ef609ed608fa621919ec1776e18901900a
Beko <bekoberk5361@gmail.com>
2024-01-30 16:46:56 +0300

    git commit. Usage: git commit -m “[added new comment]”

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import random
 from logging.config import listen
 import speech_recognition as sr
 import pyttsx3
@@ -8,8 +9,7 @@ import wikipedia
 import wolframalpha
 import time
 import winsound
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+
 
 # speech engine initialization
 engine = pyttsx3.init()
@@ -21,13 +21,17 @@ activationWord = 'jarvis'
 # Youtube music link
 ytMusiclink = r"https://music.youtube.com/playlist?list=PL4LtaZuYCvhsumxWF7jQj9iouTXL5zT6k"
 
+# Spotify Path
+spotiPath = r"C:\Users\bekob\AppData\Roaming\Spotify.exe"
+
+# Welcome Words
+welcomeWords = ['Hello Sir!', 'What would you like me to do today?', 'What would you like?', 'How are you sir', 'What do you want me to do right now?']
+randomWelcomeWords = random.choice(welcomeWords)
+
 # Configure browser
 # set the path
 opera_path = r"C:\Users\bekob\AppData\Local\Programs\Opera\launcher.exe"
 webbrowser.register('opera', None, webbrowser.BackgroundBrowser(opera_path))
-
-# Configure Selenium WebDriver
-driver = webdriver.Chrome()
 
 # Wolfrom alpha  client
 appId = 'HEW2PG-TQXWARELA7'
@@ -51,6 +55,7 @@ def parseCommand():
 
     try:
         print('Recognizing speech...')
+        speak('Analyzing Data')
         query = listener.recognize_google(input_speech, language='en_gb')
         print(f"The input speech was: {query}")
     except Exception as exception:
@@ -73,46 +78,16 @@ def search_wikipedia(query = ''):
         print(wikiPage.title)
         wikiSummary = str(wikiPage.summary)
         return wikiSummary
-
-#wolframAlpha
-"""
-
-#def search_wolframAlpha
-#def search_wolframAlpha(query = ''):
-    #response = wolfromClient.query(query)
-
-    # @succes: wolfram Alpha was able to resolve the query
-    # @numpods: Number of results. This can also contain subpods
-    # @pod: List of results. This can also cpntain subpods
-    #if response['@succes'] == 'false':
-        #return 'Could not computer'
-
-    # Query resolved
-    else:
-        result = ''
-        # Question
-        pod0 = response['pod'][0]
-
-        pod1 = response['pod'][1]
-        #May contain the answer, has the highest confidence value
-        #If it's primary, or has the title of result or definition, then it's the offical result
-        if(('result') in pod1['@title'].lower()) or (pod1.get('@primary', 'false') == 'true') or ('definition' in pod1['@title'].lower()):
-
-"""
-
 #Main loop
 if __name__ == '__main__':
     speak('All systems nominal.')
+    speak(randomWelcomeWords)
 
     while True:
         # Parse as a list
         query = parseCommand().lower().split()
         if query[0] == activationWord:
             query.pop(0)
-
-            #list commands
-            #if query[0] == 'how' or query[1] == 'are' or query[2] == 'you':
-                #speak('Everything is okey sir.what do you want me to do')
 
             # Navigation
             if query[0] == 'go' and query[1] == 'to':
@@ -126,30 +101,19 @@ if __name__ == '__main__':
                 speak('Querying the universal DataBank.')
                 speak(search_wikipedia(query))
 
-            # Note taking
-            if query[0] == 'take note':
-                speak('Ready to record your note.')
-                newNote = parseCommand().lower()
-                now = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-
-                with open('note_%s.txt' % now, 'w') as newFile:
-                    newFile.write(newNote)
-                speak('Note Written')
-            # Snooze
-            if query[0] == 'one' or query[1] == 'minute':
-                speak('Going to sleep for 1 minute')
-                time.sleep(60)
             # Play Music
             if query[0] == 'play' or query[1] == 'music':
-                speak('Opening Youtube Music Sir')
-                webbrowser.get('opera').open_new('https://music.youtube.com/playlist?list=PL4LtaZuYCvhsumxWF7jQj9iouTXL5zT6k')
+                speak('Opening Spotify Sir')
+                os.open(spotiPath)
             # Search Eray on Youtube
-            if query[0] == 'start' or query[1] == 'video':
+            if query[0] == 'funny' or query[1] == 'videos':
                 speak('Opening Eray Sir')
                 webbrowser.get('opera').open_new("https://www.youtube.com/@erayozkenar/videos")
+            # Translate
+            if query[0] == 'translate':
+                speak('Opening translater')
+
             # EXIT
             if query[0] == 'exit':
                 speak('Goodbye Sir')
                 break
-            if query[0] == 'hello':
-                os.open(r"E:\Ben burgerking söyledim ihtiyar.m4a")
